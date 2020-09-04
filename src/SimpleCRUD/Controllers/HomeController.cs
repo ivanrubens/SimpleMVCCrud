371e4fe8ed4c2fd672aec43c0600f00adb569e48
ivan.martins <ivanrubens@gmail.com>
2020-09-04 15:44:23 -0300

    Refatoração do nome do projeto; Adição do IP e ID do host; Pequenos ajustes.

---
+++ src/SimpleCRUD/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SimpleCRUD.Models;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+
+namespace SimpleCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string remoteIpAddress = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            string connectionId = Request.HttpContext.Connection.Id.ToString();
+
+            ViewBag.RemoteIpAddress = remoteIpAddress;
+            ViewBag.ConnectionId = connectionId;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
